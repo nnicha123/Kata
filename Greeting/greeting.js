f9aa1013e9b68ea37aa3576cba3672ec070370d0
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-15 16:57:10 +0700

    started greeting tdd

---
+++ Greeting/greeting.js
@@ -0,0 +1,23 @@
+const { argv } = require("process")
+
+greet = (name) => {
+    if (!name) return 'Hello, my friend.'
+    else if (name.length == 1) {
+        if (name.toUpperCase() === name) return `HELLO, ${name}!`
+        return `Hello, ${name}.`
+    }
+    else if (name.length === 2) return `Hello, ${name[0]} and ${name[1]}.`
+
+    else if (name.length >= 2) {
+        let str = ''
+        for (let i = 0; i < name.length - 1; i++) {
+            str += (' ' + name[i] + ',')
+        }
+        return `Hello ${str} and ${name[name.length - 1]}`
+    };
+}
+console.log(greet('Bob'))
+console.log(greet())
+console.log(greet('JERRY'))
+console.log(greet(["Jill", "Jane"]))
+console.log(greet(["Amy", "Brian", "Charlotte"]))
